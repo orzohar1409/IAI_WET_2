359afbbac93620e14810dcb1743e973cb0266440
orzohar <or.zohar@campus.technion.ac.il>
2025-06-13 17:48:23 +0300

    first commit

--- submission.py
+++ submission.py
@@ -8,23 +8,38 @@ import random
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
     robot = env.get_robot(robot_id)
     other_robot = env.get_robot((robot_id + 1) % 2)
-    heuristic_value = float('-inf')
-    # Check the closest package for the robot
-    credit_factor = max(1, robot.credit)
+
+    # We'll build the heuristic step by step
+    heuristic_value = 0
+
+    credit = robot.credit
+    battery = robot.battery
+    credit_factor = max(1, credit)
+
     if robot.package is None:
+        # Not holding a package: go get one
         closest_package_distance = float('inf')
         for package in env.packages:
             if package.on_board:
                 my_distance = manhattan_distance(robot.position, package.position)
-                other_robot_distance = manhattan_distance(other_robot.position, package.position)
-                if my_distance < other_robot_distance:
+                other_distance = manhattan_distance(other_robot.position, package.position)
+                # Prefer packages we can reach first
+                if my_distance < other_distance:
                     closest_package_distance = min(closest_package_distance, my_distance)
-        heuristic_value = (1 / (closest_package_distance + 1)) * credit_factor * 3
-    else:
-        distance_to_drop_off = manhattan_distance(robot.position, robot.package.destination)
-        package_reward = manhattan_distance(robot.package.position, robot.package.destination) * 2
-        heuristic_value = package_reward / (distance_to_drop_off + 1) * credit_factor
 
+        if closest_package_distance < float('inf'):
+            heuristic_value += ((1 / (closest_package_distance + 1)) + credit_factor )# weight can be tuned
+    else:
+        # Holding a package: go to drop-off
+        distance_to_drop_off = manhattan_distance(robot.position, robot.package.destination)
+        package_value = manhattan_distance(robot.package.position, robot.package.destination) * 2
+
+        # Encourage going toward drop-off
+        heuristic_value += (package_value / (distance_to_drop_off + 1)) * 5  # reward for progress
+
+        # Strong bonus if standing on drop-off location and able to drop
+        if robot.position == robot.package.destination:
+            heuristic_value = 1000  # huge bonus to encourage actually dropping it off
 
     return heuristic_value
 
