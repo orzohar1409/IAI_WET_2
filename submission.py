7f8f2c47b8f5f9b9530c12723d71f0ac32600b54
orzohar <or.zohar@campus.technion.ac.il>
2025-06-13 17:34:37 +0300

    first commit

--- submission.py
+++ submission.py
@@ -3,9 +3,30 @@ from WarehouseEnv import WarehouseEnv, manhattan_distance
 import random
 
 
+
 # TODO: section a : 3
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
-    pass
+    robot = env.get_robot(robot_id)
+    other_robot = env.get_robot((robot_id + 1) % 2)
+    heuristic_value = float('-inf')
+    # Check the closest package for the robot
+    credit_factor = max(1, robot.credit)
+    if robot.package is None:
+        closest_package_distance = float('inf')
+        for package in env.packages:
+            if package.on_board:
+                my_distance = manhattan_distance(robot.position, package.position)
+                other_robot_distance = manhattan_distance(other_robot.position, package.position)
+                if my_distance < other_robot_distance:
+                    closest_package_distance = min(closest_package_distance, my_distance)
+        heuristic_value = (1 / (closest_package_distance + 1)) * credit_factor * 3
+    else:
+        distance_to_drop_off = manhattan_distance(robot.position, robot.package.destination)
+        package_reward = manhattan_distance(robot.package.position, robot.package.destination) * 2
+        heuristic_value = package_reward / (distance_to_drop_off + 1) * credit_factor
+
+
+    return heuristic_value
 
 class AgentGreedyImproved(AgentGreedy):
     def heuristic(self, env: WarehouseEnv, robot_id: int):
